c91f0e4864c711dab457fda2c2799c0e1cc3d724
Yashpal singh <yashpalsingh9rx@outlook.com>
2024-06-02 17:07:19 +0530

    checking

--- client/src/component/RequireAuth.tsx
+++ client/src/component/RequireAuth.tsx
@@ -1,10 +1,6 @@
 import {Outlet} from "react-router-dom";
 
 const RequireAuth = () => {
-<<<<<<< HEAD
-=======
->>>>>>> c2417e97e9bbf777b2509337286bedce8990563e
-    // here i will work on react query
     return <Outlet/>
 }
 export default RequireAuth;
